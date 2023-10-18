d70fd893065a7a16e5343fa86dc3032d76f31d07
srijan shankar dubey <srijanshankardubey99@gmail.com>
2023-10-18 18:07:26 +0530

    bestseller product link correction

--- src/components/slider/BestSellerSlider.jsx
+++ src/components/slider/BestSellerSlider.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 
-
 import Slider from "react-slick";
 import apiConfig from "../../config/apiConfig";
 import { useShoppingCart } from "../../context/ShoppingCartContext";
+import { Link } from "react-router-dom";
 const BestSellerSlider = () => {
   const { handleAddRemoveWishlist, AddToCart } = useShoppingCart();
   const settings = {
@@ -59,40 +59,52 @@ const BestSellerSlider = () => {
   return (
     <div className="slick-sliders products-list sestsellers grid">
       <Slider {...settings}>
-        {data.map(product => (
+        {data.map((product) => (
           <div className="item item-product slick-slide">
             <div className="products-entry clearfix product-wapper">
               <div className="products-thumb">
                 <div className="product-thumb-hover">
-                  <a href="#">
+                  {console.log("test product", product)}
+                  <Link
+                    to={`/product-details?product_id=${product?.product_gallery[0]?.product_id}`}
+                  >
                     <img
                       width={600}
                       height={600}
-                      src={product.image_path?.replace('gallery', `${product?.thumbnail}`)}
-                      style={{ height: '328px', objectFit: 'contain' }}
+                      src={product.image_path?.replace(
+                        "gallery",
+                        `${product?.thumbnail}`
+                      )}
+                      style={{ height: "328px", objectFit: "contain" }}
                       className="post-image"
                       alt
                     />
                     <img
                       width={600}
                       height={600}
-                      style={{ height: '328px', objectFit: 'contain' }}
-                      src={product.image_path?.replace('gallery', `${product?.hover_thumbnail}`)}
+                      style={{ height: "328px", objectFit: "contain" }}
+                      src={product.image_path?.replace(
+                        "gallery",
+                        `${product?.hover_thumbnail}`
+                      )}
                       className="hover-image back"
                       alt
                     />
-                  </a>
+                  </Link>
                 </div>
                 <div className="product-button">
                   <div className="btn-add-to-cart" data-title="Add to cart">
-                    <a rel="nofollow" href="#" className="product-btn button"
+                    <a
+                      rel="nofollow"
+                      href="#"
+                      className="product-btn button"
                       onClick={() => {
                         const prod = {
                           id: product.id,
                           product_name: { en: product.product_name?.en },
                           image_path: product?.images_path,
                           product_image: [`${product.product_image[0]}`],
-                          stock: product.stock
+                          stock: product.stock,
                         };
                         AddToCart(prod, 1);
                       }}
@@ -101,18 +113,21 @@ const BestSellerSlider = () => {
                     </a>
                   </div>
                   <div className="btn-wishlist" data-title="Wishlist">
-                    <button className="product-btn"
+                    <button
+                      className="product-btn"
                       onClick={(e) => {
                         const prod = {
                           id: product.id,
                           product_name: { en: product.product_name?.en },
                           image_path: product.images_path,
                           product_image: [`${product.product_image[0]}`],
-                          stock: product.stock
-                        }
-                        handleAddRemoveWishlist(e, prod)
+                          stock: product.stock,
+                        };
+                        handleAddRemoveWishlist(e, prod);
                       }}
-                    >Add to wishlist</button>
+                    >
+                      Add to wishlist
+                    </button>
                   </div>
                 </div>
               </div>
@@ -121,16 +136,14 @@ const BestSellerSlider = () => {
                   <h3 className="product-title">
                     <a href="#">{product.product_name.en}</a>
                   </h3>
-                  <span className="price">KD{product.price}</span>
+                  <span className="price">{product.price} Rs</span>
                 </div>
               </div>
             </div>
           </div>
-
-        ))
-        }
-      </Slider >
-    </div >
+        ))}
+      </Slider>
+    </div>
   );
 };
 
